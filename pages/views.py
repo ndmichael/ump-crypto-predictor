6305a9e66720a8cd692d1e2ff516c3d2570f1834
msani555 <mahmudribadu3@gmail.com>
2024-12-12 16:43:32 +0100

    pages/templates

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': 'UMP - Predictor'
+    }
+    return render(request, "pages/index.html", context)
