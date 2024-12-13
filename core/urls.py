6a2bfdf1c8a8cde63ec2aef0f2418c74eaf6acfa
msani555 <mahmudribadu3@gmail.com>
2024-12-13 07:54:26 +0100

    allauth and user app urls

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('user/', include('users.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
