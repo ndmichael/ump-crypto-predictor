da2b9dc5340357599554395a72cf7b874e7f6cf2
ndmichael <ukejemicheal@gmail.com>
2024-12-16 17:41:27 +0100

    Added predictor urls

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('user/', include('users.urls')),
+    path('user/', include('predictor.urls')),
     path('accounts/', include('allauth.urls')),
 ]
