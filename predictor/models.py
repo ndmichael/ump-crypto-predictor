e2c00b709d72c77a7fd88ab46f33839b6f69d656
msani555 <mahmudribadu3@gmail.com>
2024-12-16 01:53:09 +0100

    crypto models added

--- predictor/models.py
+++ predictor/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class CryptoPair(models.Model):
+    symbol = models.CharField(max_length=10, unique=True) # "BTCUSDT"
+    name = models.CharField(max_length=50) # "BTC/USDT"
+    timeframe = models.CharField(max_length=5)      # e.g., 1m, 5m
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.symbol
+
+
+class Prediction(models.Model):
+    pair = models.ForeignKey(CryptoPair, on_delete=models.CASCADE)
+    predicted_price = models.FloatField()
+    volume = models.FloatField()
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.crypto_symbol.symbol} ({self.time_frame}): {self.predicted_price}"
+
