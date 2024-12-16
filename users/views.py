dd430f2cc84706d5dcd2507ba0a47a0af5d3ff2e
msani555 <mahmudribadu3@gmail.com>
2024-12-16 01:56:09 +0100

    Pass form to view

--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from predictor.forms import CryptoPredictionForm
 
 
 @login_required
 def user_dashboard(request):
+    crypto_form = CryptoPredictionForm
     context ={
-        "title": "user dashboard"
+        "title": "user dashboard",
+        "crypto_form": crypto_form
     }
     return render(request, "users/user_dashboard.html", context)
 
