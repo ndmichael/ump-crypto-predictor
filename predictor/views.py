82e0bc6591d0690ce974b2a1dc759bdeb873ff79
ndmichael <ukejemicheal@gmail.com>
2024-12-16 17:43:27 +0100

    view stage one added

--- predictor/views.py
+++ predictor/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 
+def prediction_result(request):
+
+
+    context = {
+        "title": "prediction result"
+    }
+    return render(request, 'predictor/predictor_result.html', context)
