7fc6e87aa73e447d3d36c7ba37f1ef7743cbd1f6
ndmichael <ukejemicheal@gmail.com>
2024-12-16 17:41:48 +0100

    Return crypto and predictor model

--- predictor/admin.py
+++ predictor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CryptoPair, Prediction
 
-# Register your models here.
+admin.site.register(CryptoPair)
+# admin.site.register(Prediction)
+
+@admin.register(Prediction)
+class PredictionAdmin(admin.ModelAdmin):
+    list_display = ('pair', 'user', 'predicted_price', 'volume', 'timestamp')
