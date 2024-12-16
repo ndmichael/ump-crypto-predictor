48d67f2c19a48a553137661562f6c327d0174b52
ndmichael <ukejemicheal@gmail.com>
2024-12-16 17:46:12 +0100

    return custom user in admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from users.models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+# admin.site.register(CustomUser)
+# admin.site.register(Profile)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    search_fields = ['username', 'first_name', 'last_name',]
+    list_display = ('id', 'username', 'first_name', 'last_name',)
+    filter_horizontal = ('groups', 'user_permissions')
+    fieldsets = (
+        ('User', {'fields': ('username', 'password')}),
+        ('Personal Informations', {'fields': (
+            'first_name',
+            'last_name',
+            'email',
+        )}),
+        ('Permissions', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'groups',
+            'user_permissions'
+        )}),
+    )
