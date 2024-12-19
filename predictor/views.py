589d23a33c2f7588ef07f917c952c252b14145ee
ndmichael <ukejemicheal@gmail.com>
2024-12-19 16:47:21 +0100

    return all predictions ot view

--- predictor/views.py
+++ predictor/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Prediction
 
 
 def prediction_result(request):
@@ -11,8 +12,10 @@ def prediction_result(request):
 
 def predictions(request):
 
+    predictions = Prediction.objects.all().order_by("-timestamp")
 
     context = {
-        "title": "predictions"
+        "title": "predictions",
+        "predictions": predictions,
     }
     return render(request, 'predictor/predictions.html', context)
