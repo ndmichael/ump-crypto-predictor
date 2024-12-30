c07621904b8e328cf266ee22d77213e487835120
ndmichael <ukejemicheal@gmail.com>
2024-12-30 11:48:09 +0100

    Logic to get the specific prediction and return it to the screen

--- predictor/views.py
+++ predictor/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Prediction
+from django.contrib import messages
 
 
 def prediction_result(request):
 
+    prediction_id = request.GET.get("prediction_id")
+    if not prediction_id:
+        messages.error(request, "No prediction found.")
+        return redirect("user_dashboard")
+
+    try:
+        prediction = Prediction.objects.get(id=prediction_id, user=request.user)
+    except Prediction.DoesNotExist:
+        messages.error(request, "Prediction not found.")
+        return redirect("user_dashboard")
 
     context = {
-        "title": "prediction result"
+        "title": "prediction result",
+        "prediction": prediction,
     }
     return render(request, 'predictor/predictor_result.html', context)
 
