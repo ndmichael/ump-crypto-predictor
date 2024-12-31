dd8bbc765f4df142d8115ac5ca699b08f3de30be
ndmichael <ukejemicheal@gmail.com>
2024-12-31 16:39:12 +0100

    URLs for the predictions result

--- predictor/urls.py
+++ predictor/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from .views import prediction_result, predictions
 
 urlpatterns = [
-    path('dashboard/results', prediction_result, name="prediction_result"),
+    path('dashboard/result/<int:prediction_id>/', prediction_result, name="prediction_result"),
     path('dashboard/predictions', predictions, name="predictions"),
 ]
