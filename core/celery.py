6cec061445ebe3b90956808a378d614142d68a17
ndmichael <ukejemicheal@gmail.com>
2025-01-05 11:11:28 +0100

    Created a celery.py worker

---
+++ core/celery.py
@@ -0,0 +1,30 @@
+# celery.py
+
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Create a Celery instance and configure it using Django settings.
+app = Celery('core')
+
+# Load task modules from all registered Django app configs.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Autodiscover tasks in all installed apps.
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
+
+# app.conf.beat_schedule = {
+#     'retrain-every-24hrs': {
+#         'task': 'clients.tasks.calculate_interest',
+#         # 'schedule': crontab(minute=0, hour=0),  # Run the task daily at midnight
+#         'schedule': crontab(minute='*/1'),
+#     },
+# }
