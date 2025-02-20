bb3f7c3046f871c7fbe4898d9e50a64a79f525a1
ndmichael <ukejemicheal@gmail.com>
2025-02-20 11:48:23 +0100

    models adjusted to include newer fields

--- predictor/models.py
+++ predictor/models.py
@@ -19,6 +19,11 @@ class Prediction(models.Model):
     pair = models.ForeignKey(CryptoPair, on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1)
     predicted_price = models.DecimalField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=20, decimal_places=8, default=0.00)
+    stop_loss = models.DecimalField(max_digits=20, decimal_places=8, default=0.00)
+    take_profit = models.DecimalField(max_digits=20, decimal_places=8, default=0.00)
+    confidence_score = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    signal = models.CharField(max_length=4, choices=[("BUY", "BUY"), ("SELL", "SELL"), ("HOLD", "HOLD")], default="HOLD")  # Buy/Sell Signal
     volume = models.DecimalField(max_digits=10, decimal_places=2)
     timestamp = models.DateTimeField(default=timezone.now)
 
