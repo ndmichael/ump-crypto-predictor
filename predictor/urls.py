93d77ac25851568e83d9f430d2869a92b4d1f188
ndmichael <ukejemicheal@gmail.com>
2025-10-30 15:52:22 +0100

    url to predict form

--- predictor/urls.py
+++ predictor/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import prediction_result, predictions, user_settings
+from .views import prediction_result, predictions, user_settings, predict
 
 urlpatterns = [
+    path('dashboard/predict/', predict, name="user_predict"),
     path('dashboard/result/<int:prediction_id>/', prediction_result, name="prediction_result"),
     path('dashboard/predictions', predictions, name="predictions"),
     path('dashboard/profile/settings', user_settings, name="user_settings"),
